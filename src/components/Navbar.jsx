f59d3e5a3816361b071ba816047e3beb9cd3dd2a
ewwmonya <savvymallen85@gmail.com>
2025-04-26 00:36:33 -0400

    nav kind of made but i was tired

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,24 @@
+import Link from 'next/link';
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className='flex flex-nowrap mx-7 items-center justify-between md:justify-between md:mx-32'>
+			<h1 className='text-2xl font-black'>
+				Sea <span className='text-sky-700'>Elite</span>
+			</h1>
+			<div className='flex justify-between gap-3 '>
+				<Link href={'#'}>
+					<button className='capitalize pointer text-lg text-gray-700'>
+						products
+					</button>
+				</Link>
+				<Link href={'#'}>
+					<button className='capitalize pointer text-lg text-gray-700'>
+						catagories
+					</button>
+				</Link>
+			</div>
+		</div>
+	);
 };
 export default Navbar;
---
+++ src/lib/data.js
@@ -0,0 +1,8 @@
+export const categories = [
+	{ id: 1, name: 'BCD Packages', slug: 'bcd-packages' },
+	{ id: 2, name: 'Regulators', slug: 'regulators' },
+	{ id: 3, name: 'Wetsuits', slug: 'wetsuits' },
+	{ id: 4, name: 'Snorkeling', slug: 'snorkeling' },
+	{ id: 5, name: 'Accessories', slug: 'accessories' },
+	{ id: 6, name: 'Masks & Fins', slug: 'masks-fins' },
+];

33c9d081f95db62fd51eb854d3542421df7cf68c
ewwmonya <savvymallen85@gmail.com>
2025-04-29 01:30:35 -0400

    done for night.

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import {
 	CardContent,
 } from '@/components/ui/card';
 import { Hero } from './../components/homePage/hero';
+import { Button } from '@/components/ui/button';
 const items = [
 	{ id: 1, name: 'scout BCD' },
 	{ id: 2, name: 'scout BCD' },
@@ -17,25 +18,34 @@ export default function Home() {
 		<div className=''>
 			<Hero />
 			{/* Cards Prob Top Gear */}
-			<section tabIndex={5}>
-				<div className='cardContainer md:max-w-4xl my-0 mx-auto max-w-5xl'>
-					<div className='grid grid-cols-3 m-4 gap-x-2 md:gap-x-2 '>
+			<section
+				tabIndex={5}
+				className=' mt-12 cardContainer md:max-w-4xl my-0 md:mx-auto max-w-5xl mx-4'
+			>
+				<h5 className=' text-2xl font-bold tracking-wide'>Featured Gear</h5>
+				<div className=''>
+					<div className='grid md:grid-cols-3 grid-cols-2 m-4 gap-4 md:gap-x-2 justify-center'>
 						{items.map((item) => {
 							return (
-								<Card key={item.id} className='md:max-w-60'>
+								<Card key={item.id} className='md:max-w-60  flex flex-col'>
 									{/* Img here */}
-									<CardHeader>
+									<CardHeader className='flex-col justify-center'>
 										<div className='w-24 h-24 md:w-48 md:h-48 bg-gray-200 border-2 border-gray-100'></div>
 									</CardHeader>
 									<CardContent>
 										<CardTitle>{item.name}</CardTitle>
+										<CardDescription className=''>
+											Lorem ipsum dolor sit amet consectetur adipisicing elit.
+											Voluptatum, odio!
+										</CardDescription>
 									</CardContent>
 									{/* bottom of card */}
-									<CardFooter>
-										<CardDescription>
-											{/* price below */}
-											<p>$99.99</p>
-										</CardDescription>
+									<CardFooter className='justify-around md:flex-nowrap flex-col md:flex-row'>
+										{/* price below */}
+										<p>$99.99</p>
+										<Button className='bg-sky-700 hover:bg-sky-50 hover:text-sky-700 hover:border-[2px] hover:border-sky-700'>
+											Learn More
+										</Button>
 									</CardFooter>
 								</Card>
 							);
